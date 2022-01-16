dc84abc7866397fb6dc1c3486e94ef0ad87af279
Rahul <rahuljayaraman8@gmail.com>
2022-01-16 18:28:02 +0530

    added the toast

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ToastContainer } from 'react-toastify';
+
+const Toast = () => {
+    return (
+        <div>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default Toast
--- src/pages/CurrencyConverter.jsx
+++ src/pages/CurrencyConverter.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
-import './CurrencyConverter.css'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import { } from 'react-papaparse'
+import './CurrencyConverter.css'
+import Buttons from '../components/Button/ButtonUpload';
+
+import { CSVLink } from "react-csv";
 import { parse } from 'papaparse'
 import axios from 'axios'
+import Toast from '../components/Toast/Toast';
+
 
 
 
 const CurrencyConverter = () => {
     const [details, setDetails] = useState([])
+    const [fileName, setFileName] = useState('')
     const [currencies, setCurrencies] = useState([])
     const [rates, setRates] = useState([])
-    const [fromCurrency, setfromCurrency] = useState('')
     const [toCurrency, setToCurrency] = useState('')
-
-
-
-
-
+    const [newData, setNewData] = useState()
 
     useEffect(() => {
         if (details.length > 0) {
@@ -38,62 +40,84 @@ const CurrencyConverter = () => {
     useEffect(() => {
         let inDollars
         let inRequiredCurrency
+        const current = new Date();
+        const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
         if (toCurrency.length > 0) {
             const newData = details.map((obj) => {
-                console.log(rates[obj.Currency], obj.Amount);
-                inDollars = parseInt(obj.Amount) / rates[obj.Currency]
-                console.log(inDollars);
-                inRequiredCurrency = inDollars * rates[toCurrency]
-                return { ...obj, "Converted Currency": toCurrency, "Converted Amount": inRequiredCurrency }
+                if (obj.Currency && obj.Amount) {
+                    inDollars = parseInt(obj.Amount) / rates[obj.Currency]
+                    inRequiredCurrency = inDollars * rates[toCurrency]
+                    return { ...obj, "Converted Currency": toCurrency, "Converted Amount": inRequiredCurrency, "Converted On": date }
+                } else {
+                    return { ...obj }
+                }
+
             })
+            setNewData(newData)
             console.log(newData);
         }
     }, [toCurrency])
 
 
-    const handletheDrop = (e) => {
+    const handletheFile = (e) => {
         e.preventDefault()
-        Array.from(e.dataTransfer.files)
-            .filter((file) => file.type === "application/vnd.ms-excel")
-            .forEach(async (file) => {
-                const data = await file.text()
-                const result = parse(data, { header: true })
-                setDetails([...result.data])
-            })
+        console.log(e.target.files)
+        if (e.target.files[0].type === "application/vnd.ms-excel") {
+            setFileName(e.target.files[0].name)
+            Array.from(e.target.files)
+                .filter((file) => file.type === "application/vnd.ms-excel")
+                .forEach(async (file) => {
+                    const data = await file.text()
+                    const result = parse(data, { header: true })
+                    setDetails([...result.data])
+                })
+        } else {
+            toast.error("Invalid file format")
+        }
     }
+    console.log(details);
+    console.log(newData);
 
-    const handletheDrag = (e) => {
-        e.preventDefault()
+    const headers = [
+        { label: 'Name', key: "Name" },
+        { label: 'Currency', key: "Currency" },
+        { label: 'Amount', key: "Amount" },
+        { label: 'Transaction Date', key: "Transaction Date" },
+        { label: 'Converted Currency', key: "Converted Currency" },
+        { label: 'Converted Amount', key: "Converted Amount" },
+        { label: 'Converted On', key: "Converted On" }
+    ];
+
+    const csvReports = {
+        filename: fileName,
+        headers: headers,
+        data: newData
     }
-
-
-
-
-
-
 
     return (
-        <div className='main-container'>
-            <h3>csv currency converter</h3>
-            <div className="container-wrapper">
-                <div className='uploader'
-                    onDrop={handletheDrop}
-                    onDragOver={handletheDrag}
-                >
-                    <p>Drop your csv file here</p>
+        <>
+            <div className='main-container'>
+                <h2>csv currency converter</h2>
+                <div className="container-wrapper">
+                    <Buttons handletheFile={handletheFile} />
+                    <div className='select-wrapper'>
+                        <h4>Convert to:</h4>
+                        <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+                            {
+                                currencies.map((currency) => (
+                                    <option key={currency} value={currency}> {currency}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    {
+                        newData ? <CSVLink className='csv' {...csvReports} >Download</CSVLink> : ''
+                    }
+
                 </div>
-                <div className='select-wrapper'>
-                    <h4>Convert to:</h4>
-                    <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
-                        {
-                            currencies.map((currency) => (
-                                <option key={currency} value={currency}> {currency}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-        </div >
+            </div >
+            <Toast />
+        </>
     )
 }
 
